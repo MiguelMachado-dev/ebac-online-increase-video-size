6b9adb4d5b1aeb6ea7c6038e4c390891cf5dcdb3
Miguel Machado <miiguellmp@gmail.com>
2022-11-12 17:56:14 -0300

    Update ebacIncreaseVideoSize.user.js

--- ebacIncreaseVideoSize.user.js
+++ ebacIncreaseVideoSize.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         EBAC Online - Bigger video screen size
 // @namespace    https://github.com/MiguelMachado-dev/ebac-online-increase-video-size
-// @version      0.2.2
+// @version      0.2.3
 // @description  Get a bigger video screen size on EBAC lessons
 // @updateURL    https://github.com/MiguelMachado-dev/ebac-online-increase-video-size/raw/main/ebacIncreaseVideoSize.user.js
 // @downloadURL  https://github.com/MiguelMachado-dev/ebac-online-increase-video-size/raw/main/ebacIncreaseVideoSize.user.js
 // @author       https://github.com/MiguelMachado-dev
-// @match        https://lms.ebaconline.com.br/lesson/*
+// @match        https://lms.ebaconline.com.br/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ebaconline.com.br
 // @run-at       document-end
 // @grant        none
